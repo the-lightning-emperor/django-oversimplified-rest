d70b936bf6cec23a55ea2f4c0e960a4e173ccdca
the-lighning-emperor <thelesserlovedemail.com>
2022-07-15 12:35:56 +0530

    add and show using serializers

---
+++ oversimplified_rest/api/serializers.py
@@ -0,0 +1,8 @@
+from django.db.models import fields
+from rest_framework import serializers
+from base.models import Item
+
+class ItemSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Item
+        fields = '__all__'
---
+++ oversimplified_rest/api/views.py
@@ -0,0 +1,21 @@
+from matplotlib.pyplot import get
+from rest_framework.response import Response
+
+from rest_framework.decorators import api_view
+
+from base.models import Item
+
+from .serializers import ItemSerializers
+
+@api_view(['GET'])
+def getData(request):
+    items = Item.objects.all()
+    serializer = ItemSerializers(items, many=True)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def addItem(request):
+    serializer = ItemSerializers(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
